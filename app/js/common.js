85a0461470d1ff9d7fe1d442b7e9487bae37d48a
shabalin Vyacheslav <svaznoy0309@gmail.com>
2020-11-18 15:48:25 +0800

    added adaptation for mobile

--- app/js/common.js
+++ app/js/common.js
@@ -1,30 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
-	const form = document.querySelector(".form")
-	const cardList = document.querySelector(".sort__container")
-	const email = document.querySelector(".form__input")
-	const formBtn = document.querySelector(".form__button")
+	const form = document.querySelector('.form')
+	const cardList = document.querySelector('.sort__container')
+	const email = document.querySelector('.form__input')
+	const formBtn = document.querySelector('.form__button')
+	const showMoreCard = document.querySelector('.sort__show')
 	const emailRegexp = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+	const goTopBtn = document.querySelector('.sort__up')
+	const menuMobile = document.querySelector('.navigation')
 	let sortItem = 0
-	let goTopBtn = document.querySelector(".sort__up")
 
 
 	// Change button and color
 	const handleCardListClick = ({target}) => {
-		if (target.classList.contains("sort__button")) {
-			target.innerText = "Продано"
-			target.style.backgroundColor = "#000"
+		if (target.classList.contains('sort__button')) {
+			target.innerText = 'Продано'
+			target.style.backgroundColor = '#000'
 			target.disabled = true
 		}
 	}
 
-	cardList.addEventListener("click", handleCardListClick)
+	cardList.addEventListener('click', handleCardListClick)
 
 	// Show more cards
-	document.querySelector('.sort__show').addEventListener('click', function() {
+	showMoreCard.addEventListener('click', function() {
 		let sortHidden = document.querySelectorAll('.sort__hidden')
-		sortHidden[sortItem].style.display = "flex";
+		sortHidden[sortItem].style.display = 'flex';
 		if(sortItem+1 === sortHidden.length){
-		  this.style.display = "none";
+		  this.innerText = 'Закончились';
+		  this.disabled - true
+		  this.style.backgroundColor = '#1f2021'
+		  this.style.color = '#fff'
 		}
 		sortItem++
 	  });
@@ -35,10 +40,10 @@ document.addEventListener('DOMContentLoaded', function(){
 		let coords = document.documentElement.clientHeight
 
 		if (scrolled > coords) {
-			goTopBtn.classList.add("sort__up_show")
+			goTopBtn.classList.add('sort__up_show')
 		}
 		if (scrolled < coords) {
-			goTopBtn.classList.remove("sort__up_show")
+			goTopBtn.classList.remove('sort__up_show')
 		}
 	}
 
@@ -49,10 +54,10 @@ document.addEventListener('DOMContentLoaded', function(){
 		 }
 	}
 
-	window.addEventListener("scroll", trackScroll)
-	goTopBtn.addEventListener("click", backToTop)
+	window.addEventListener('scroll', trackScroll)
+	goTopBtn.addEventListener('click', backToTop)
 
-	form.addEventListener("submit", function(e){
+	form.addEventListener('submit', function(e){
 		e.preventDefault()
 	})
 
@@ -62,16 +67,31 @@ document.addEventListener('DOMContentLoaded', function(){
 	}
 
 	function updateInput() {
-		if (validateEmail(email.value) || email.value === "") {
-			email.style.border = "2px solid green"
+		if (validateEmail(email.value)) {
+			email.style.border = '2px solid green'
 			formBtn.disabled = false		
 		} else {
-			email.style.border = "2px solid red"
+			email.style.border = '2px solid red'
 			email.style.borderColor = 'red';
 			formBtn.disabled = true
 		} 	
 	}
 
-	email.addEventListener("input", updateInput)
+	email.addEventListener('input', updateInput)
+
+	 // Open mobile menu
+	 document.querySelector('.nav__menu').addEventListener('click', function() {
+		this.classList.toggle('nav__mobile--active')
+		document.querySelector('.mobile').classList.toggle('mobile--active');
+	  })
+
+	   // Add color for mobile menu
+  window.addEventListener('scroll', function(e) {
+    if(this.pageYOffset >= 120) {
+		menuMobile.classList.add('fixed');
+    } else {
+		menuMobile.classList.remove('fixed');
+    }
+  })
 });
   
